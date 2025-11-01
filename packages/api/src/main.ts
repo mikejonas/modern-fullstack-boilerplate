7c57db7fa39fb447e4f7f3adb0f2bb60bad6507f
Mike Jonas <motion@gmail.com>
2025-11-01 13:32:40 -0700

    init

---
+++ packages/api/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3001;
+  await app.listen(port);
+  console.log(`🚀 API running on http://localhost:${port}`);
+}
+bootstrap().catch((error) => {
+  console.error('Error starting Nest application:', error);
+  process.exit(1);
+});
